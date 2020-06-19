242f87554eea49995d01ab881ca031607422de1f
lijiaqun <1274854641@qq.com>
2020-06-19 18:23:49 +0800

    修改：...

--- app/src/main/java/com/mercury/demo/rxjava/RxJavaActivity.java
+++ app/src/main/java/com/mercury/demo/rxjava/RxJavaActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaActivity extends Activity {
 
@@ -19,5 +26,61 @@ public class RxJavaActivity extends Activity {
     private void test(){
         Observable<LoveReulst> observable1 = ApiHelp.getTestService().getLove(new LoveRequestBody());
         Observable<WeatherResult> observable2 = ApiHelp.getTestService().getWeather(new WeatherRequestBody());
+
+        //1.simple
+        Disposable disposable = ApiHelp.getTestService().getLove(new LoveRequestBody())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<LoveReulst>() {
+                    @Override
+                    public void accept(LoveReulst loveReulst) {
+                        //do result
+                    }
+                });
+
+        //2.flatmap
+        Disposable disposable2 = ApiHelp.getTestService().getLove(new LoveRequestBody())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Function<LoveReulst, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(final LoveReulst loveReulst) throws Exception {
+                        if(loveReulst != null){
+                            return new Observable<String>() {
+                                @Override
+                                protected void subscribeActual(Observer<? super String> observer) {
+                                      observer.onNext(loveReulst.toString());
+                                }
+                            };
+                        }
+                        return null;
+                    }
+                }).subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s)   {
+
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable)  {
+
+                    }
+                });
+
+        //3.map
+        Disposable disposable3 = ApiHelp.getTestService().getLove(new LoveRequestBody())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Function<LoveReulst, String>() {
+                    @Override
+                    public String apply(LoveReulst loveReulst) throws Exception {
+                        return null;
+                    }
+                }).subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+
+                    }
+                });
     }
 }
